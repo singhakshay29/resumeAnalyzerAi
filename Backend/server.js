fcf4a1737a4af69990f2b443555e8cef71ecb58f
akshaysingh29 <akshay2898.as@gmail.com>
2026-05-19 14:44:49 +0530

    fix: update database connection handling; ensure proper server startup and change auth API base URL to production

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const app = require("./src/app");
 const connectToDB = require("./src/config/database");
-connectToDB();
-app.listen(3000, () => {
-  console.log("Server is running on 3000 Port");
-});
+async function startServer() {
+  await connectToDB();
+
+  app.listen(3000, () => {
+    console.log("Server is running on 3000 Port");
+  });
+}
+startServer();
--- Frontend/src/feature/auth/services/auth.api.js
+++ Frontend/src/feature/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/auth",
+  baseURL: "https://resume-analyzer-ai-backend.vercel.app/api/auth",
   withCredentials: true,
 });
 export async function registerUser({ userName, email, password }) {
