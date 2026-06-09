5fa3a129c37c665e07e102d6722bab89b11e2541
akshaysingh29 <akshay2898.as@gmail.com>
2026-06-10 00:23:33 +0530

    navbar updates

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import {useNavigate} from "react-router";
+import {logoutUser} from "../feature/auth/services/auth.api";
 
 
 export const Navbar = () => {
@@ -6,25 +7,30 @@ export const Navbar = () => {
     const handleGetAllReport = () => {
         navigate("/reports");
       };
+    const handleLogout = ()=>{
+        logoutUser();
+        navigate("/");
+    }  
     
   return (
     <nav className="navbar">
     <div className="navbar-container">
-      <h1 className="navbar-logo">
-        Resume Analysis
-      </h1>
+    <div className="navbar-brand">
+  <span className="brand-icon">⚡</span>
+  <div>
+    <h1>ResumeAI</h1>
+    <span>Interview Assistant</span>
+  </div>
+</div>
   
       <div className="navbar-actions">
-        <button className="navbar-link">
-          Features
-        </button>
   
         <button className="navbar-link" onClick={()=>handleGetAllReport()}>
-          Reports
+          History
         </button>
   
-        <button className="navbar-btn">
-          Get Started
+        <button className="navbar-btn" onClick={()=>handleLogout()}>
+          Log out
         </button>
       </div>
     </div>
